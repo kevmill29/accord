d278d9c3b4fb02807da801d8aad72147f6a22f06
Kevin Emile <96067818+kevmill29@users.noreply.github.com>
2023-04-05 09:16:02 -0400

    Add files via upload

---
+++ app/store.js
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import userReducer from "../features/userSlice";
+import appReducer from "../features/appSlice";
+
+
+ const store = configureStore({
+    reducer: {
+        user: userReducer,
+        app: appReducer,
+    },
+});
+
+export default store
